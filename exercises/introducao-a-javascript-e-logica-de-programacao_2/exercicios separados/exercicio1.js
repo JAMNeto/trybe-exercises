23bffbd4e8db04c31108c4b012dd4da11e01549c
Jose Antonio Moura Neto <netomoura22@gmail.com>
2021-03-26 15:34:44 -0300

    finalizaçao do exercicio 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios separados/exercicio1.js
@@ -0,0 +1,10 @@
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+
+//1 - Criar um loop que percorra todo o array
+//2 - Fazer com que esse loop imprima os numero da array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index]);
+}
