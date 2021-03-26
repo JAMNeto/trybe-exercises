6e0e7ccda441f4413fb2704dc9a5d3d1450f5de6
Jose Antonio Moura Neto <netomoura22@gmail.com>
2021-03-26 18:17:19 -0300

    ultimo commit

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios separados/bonus.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios separados/bonus.js
@@ -11,11 +11,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let mult = [];
 
 for(let index = 0; index < numbers.length; index += 1){
-    if (numbers[index + 1]) {
-        mult.push(numbers[index] * numbers[index + 1]);
-    }
-    else {
+    if(numbers[index + 1] = null){
         mult.push(numbers[index] * 2);
+    }
+    else{
+        mult.push(numbers[index] * numbers[index + 1]);
     }
 }
 
