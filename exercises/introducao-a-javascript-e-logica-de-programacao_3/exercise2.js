311d7ed7f0eb0d4f0e441e800bec2b255024edd0
Jose Antonio Moura Neto <netomoura22@gmail.com>
2021-03-26 18:28:30 -0300

    finalizaçao do exercicio 2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise2.js
@@ -0,0 +1,8 @@
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base
+
+let n = 5;
+let ast = [];
+for(let index = 0; index < n; index +=1){
+    ast.push('*');
+    console.log(ast);
+}
