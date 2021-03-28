b3a2653748f383c6c17b9d5bbe3fa431b91a8fa0
Jose Antonio Moura Neto <netomoura22@gmail.com>
2021-03-28 19:08:14 -0300

    correçao exercicio 3 e criaçao do exercicio 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise3.js
@@ -2,13 +2,13 @@
 
 let n = 5;
 let ast = [];
+
 for(let index = 0; index < n; index += 1){
     ast.push(' ');
 }
-
-for(let index2 = (n - 1); index2 >= 0; index2 -= 1){
-    if(ast[index2] === ' '){
-    ast[index2] = '*';
-}
-console.log(ast);
+for(let index = (n - 1); index >= 0; index -= 1){
+    if(ast[index] === ' '){
+        ast[index] = '*';
+    }
+    console.log(ast);
 }
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise4.js
@@ -0,0 +1,10 @@
+//Depois, faça uma pirâmide com n asteriscos de base:
+
+let n = 5;
+let ast = [];
+for(let index = 0; index < n; index += 1){
+    ast.push(' ');
+}
+
+
+console.log(ast);
