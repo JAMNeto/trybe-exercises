90262a75f2c846f97f45f55c89a3e5b916d57d24
Jose Antonio Moura Neto <netomoura22@gmail.com>
2021-04-07 19:23:55 -0300

    conclusao do exercicio 1

---
+++ javascript-dom-eventos-e-web-storage_3/script.js
@@ -0,0 +1,36 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek() 
+  
+function createDate() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    let listDays = document.querySelector('#days');
+    for (index = 0; index < dezDaysList.length; index += 1) {
+        let day = document.createElement('li');
+        day.innerText = dezDaysList[index];
+        day.classList.add('day');
+        if (dezDaysList[index] === 24 || dezDaysList[index] === 25 || dezDaysList[index] === 31) {
+            day.classList.add('holiday');
+        }
+        if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18 || dezDaysList[index] === 25) {
+            day.classList.add('friday');
+        }
+        listDays.appendChild(day);
+    }
+    return listDays;
+}
+    listDays = createDate();
+  
+  // Escreva seu código abaixo.
+  //Exercicio 1
